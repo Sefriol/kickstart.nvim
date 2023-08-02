0d01ad827cbd6ca52a2ad6a15f6731ebd44cf925
Joakim Järvinen <joakim.jarvinen@brightly.fi>
2023-08-02 16:52:49 +0300

    feat: add personalized keymaps

--- init.lua
+++ init.lua
@@ -384,7 +384,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>/', function()
+      vim.keymap.set('n', '<leader>f', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
           winblend = 10,
@@ -885,7 +885,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
---
+++ lua/custom/plugins/keymaps.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<C-c>", vim.cmd.Ex, { desc = "Exit file"})
+vim.keymap.set("i", "<C-c>", "<Esc>", { desc="[C]ancel insert mode" })
+return {}

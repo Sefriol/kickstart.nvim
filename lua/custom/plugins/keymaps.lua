a6bb9edca8ef6e8fd17e2662e11e0a709fe465cb
Sefriol <georgepatton1945@hotmail.com>
2023-10-05 00:31:47 +0300

    Add key remap for selection search

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "<C-c>", vim.cmd.Ex, { desc = "Exit file"})
 vim.keymap.set("i", "<C-c>", "<Esc>", { desc="[C]ancel insert mode" })
+vim.keymap.set("x", "/", "<Esc>/\\%V", { desc="Search in selection" })
 return {}
